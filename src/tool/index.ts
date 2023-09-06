95dfd5e808fc911d28191c1e1a0ae6cf93c7437b
Anthony <anton@prominence.tech>
2023-09-06 17:30:23 +0200

    rm unused NotificationTool;

--- src/tool/index.ts
+++ src/tool/index.ts
@@ -1,12 +1,10 @@
 import CalculationTool from './calculation.tool';
-import NotificationTool from './notification.tool';
 import TradeTool from './trade.tool';
 import { PrintTool } from './print.tool';
 
 export {
   CalculationTool,
   TradeTool,
-  NotificationTool,
   PrintTool,
 };
 
--- src/tool/notification.tool.ts
+++
@@ -1,9 +0,0 @@
-import { PubNubChannelDestinationEnum, PubNubChannelPrefixEnum, PubNubChannelTypeEnum } from '@kitzen/data-transfer-objects';
-
-class NotificationTool {
-  public static getChannelName(prefix: PubNubChannelPrefixEnum, type: PubNubChannelTypeEnum, destination: string | PubNubChannelDestinationEnum): string {
-    return `${prefix}.${type}.${destination}`;
-  }
-}
-
-export default NotificationTool;
