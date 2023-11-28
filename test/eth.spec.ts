60ebf27cb69a3b8a06d9ba4b8983e3459bd0a3b6
neese <temanharosh@gmail.com>
2023-11-28 16:11:28 +0200

    fix: complete signMessage test

--- test/eth.spec.ts
+++ test/eth.spec.ts
@@ -17,9 +17,10 @@ describe('Ethereum', () => {
     });
 
     describe('signMessage', () => {
-        it('should sign message', () => {
+        it('should sign message', async () => {
             const privateKey = 'xprvA2af7XVoXiG6XgrR51AtpsaMkSwXks8x97yztoEzteaB3VReDxv2RMx7HukgKNR4nTCniVdri2Kq6U8Uu294HQDZm9FKrrKTZDWJAeokaZv';
-            ethereum.signMessage('dcf55538-a69a-4b24-a7c7-bbbba4ec19f3', privateKey)
+            const sign = await ethereum.signMessage('dcf55538-a69a-4b24-a7c7-bbbba4ec19f3', privateKey)
+            expect(sign).toStrictEqual('0x8cd096afc562c12acb66902c1c7d329792ff138ed5be3369fd70375f77eeb27058146d041aae6a7a692826a8aea2d37e1bc8ed60c47bff521c522cbb27062b3d1c')
         });
     });
 });
