ee90628c4c734b8f24bf1d21c91350e8873d7daf
neese <temanharosh@gmail.com>
2023-11-29 10:50:52 +0200

    fix: usage of eth private key

--- test/eth.spec.ts
+++ test/eth.spec.ts
@@ -10,17 +10,17 @@ describe('Ethereum', () => {
 
     describe('getEthAddressFromPrivateKey', () => {
         it('should return the correct address', () => {
-            const privateKey = 'xprvA2af7XVoXiG6XgrR51AtpsaMkSwXks8x97yztoEzteaB3VReDxv2RMx7HukgKNR4nTCniVdri2Kq6U8Uu294HQDZm9FKrrKTZDWJAeokaZv';
+            const privateKey = 'xprvA1MzT94xpgCNrpZcnhLxznaiBdkPnjKBxMzj8YkFZG8qAafQTW28vJhaSPb1ssf9VqwctVLTnMJ5Ex388BJKuWtf6CdAUgp8pQCuc1obxBN';
             const address = ethereum.getEthAddressFromPrivateKey(privateKey);
-            expect(address).toStrictEqual([{address: "0xE704aa04CDE541bDEA56933434bEBC101b855132", derivePath: "m/84'/0'/0'/0/0"}]);
+            expect(address).toStrictEqual([{address: "0xE704aa04CDE541bDEA56933434bEBC101b855132", derivePath: "m/44'/60'/0'/0/0"}]);
         });
     });
 
     describe('signMessage', () => {
         it('should sign message', async () => {
-            const privateKey = 'xprvA2af7XVoXiG6XgrR51AtpsaMkSwXks8x97yztoEzteaB3VReDxv2RMx7HukgKNR4nTCniVdri2Kq6U8Uu294HQDZm9FKrrKTZDWJAeokaZv';
-            const sign = await ethereum.signMessage('dcf55538-a69a-4b24-a7c7-bbbba4ec19f3', privateKey)
-            expect(sign).toStrictEqual('0x8cd096afc562c12acb66902c1c7d329792ff138ed5be3369fd70375f77eeb27058146d041aae6a7a692826a8aea2d37e1bc8ed60c47bff521c522cbb27062b3d1c')
+            const privateKey = 'xprvA1MzT94xpgCNrpZcnhLxznaiBdkPnjKBxMzj8YkFZG8qAafQTW28vJhaSPb1ssf9VqwctVLTnMJ5Ex388BJKuWtf6CdAUgp8pQCuc1obxBN';
+            const sign = await ethereum.signMessage("64cfa063-188a-4750-9b7d-593259eb2075", privateKey)
+            expect(sign).toStrictEqual('0x3537c3275fd591035f97a02ccff8b7d189cb25463d0b43b09be6b03bff6dd7c61d514bbee7fa1495505120a3d22c1e2db9fc6aa7659a4f7d6b0633c61619c2a51c')
         });
     });
 });
